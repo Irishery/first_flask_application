95f1ee83f4425d6d7b7e7f21c53dede7f5c16fd7
Irishery <markirill21@gmail.com>
2022-02-20 03:15:19 +0300

    added templates for training

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from turtle import title
+from unicodedata import name
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +7,19 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def main():
     return render_template("base.html", title="LOL")
+
+@app.route('/training/<prof>', methods=["GET"])
+def get_training(prof):
+    name: str
+    img_link: str
+
+    if prof == "doc":
+        name = "Научные симуляторы"
+        img_link = "doc_train.jpg"
+    elif prof == 'eng':
+        name = "Инженерные тренажеры"
+        img_link = "eng_train.jpg"
+    return render_template("training_info.html", name=name, img_link=img_link)
+
 
 app.run("127.0.0.1", port=8080, debug=True)
